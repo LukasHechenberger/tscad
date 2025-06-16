d994b7cc0fc22d8359b81b2ee83f721a7646a399
Lukas Hechenberger <5802656+LukasHechenberger@users.noreply.github.com>
2025-06-17 00:01:03 +0200

    style: Refactor

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { primitives, transforms, colors, booleans, extrusions } from '@jscad/modeling';
-import type { Geom3 } from '@jscad/modeling/src/geometries/types';
 import { slopedCuboid } from './lib/sloped-cuboid';
 import { mapVector, type Vector2, type Vector3 } from './lib/vectors';
+import { pattern } from './lib/modeling';
 
 // Specs: https://gridfinity.xyz/specification/
 
@@ -17,24 +17,6 @@ const gridfinity = {
   baseplatePadding: 2.15,
   baseplateRadius: 4,
 };
-
-function pattern(
-  { cells, cellSize, center = [0, 0, 0] }: { cells: Vector2; cellSize: Vector2; center?: Vector3 },
-  geometry: Geom3
-) {
-  const [x, y] = cells;
-
-  return Array.from({ length: x }, (_, i) =>
-    Array.from({ length: y }, (_, j) => {
-      const offset = [
-        center[0] + (-cells[0] / 2 + i + 0.5) * cellSize[0],
-        center[1] + (-cells[1] / 2 + j + 0.5) * cellSize[1],
-      ] as const;
-
-      return translate([...offset, center[2]], geometry);
-    })
-  ).flat();
-}
 
 const baseplateCutout = () =>
   union(
---
+++ src/lib/modeling.ts
@@ -0,0 +1,25 @@
+import type { Geom3 } from '@jscad/modeling/src/geometries/types';
+import type { Vector2, Vector3 } from './vectors';
+import { transforms } from '@jscad/modeling';
+
+// Specs: https://gridfinity.xyz/specification/
+
+const { translate } = transforms;
+
+export function pattern(
+  { cells, cellSize, center = [0, 0, 0] }: { cells: Vector2; cellSize: Vector2; center?: Vector3 },
+  geometry: Geom3
+) {
+  const [x, y] = cells;
+
+  return Array.from({ length: x }, (_, i) =>
+    Array.from({ length: y }, (_, j) => {
+      const offset = [
+        center[0] + (-cells[0] / 2 + i + 0.5) * cellSize[0],
+        center[1] + (-cells[1] / 2 + j + 0.5) * cellSize[1],
+      ] as const;
+
+      return translate([...offset, center[2]], geometry);
+    })
+  ).flat();
+}
