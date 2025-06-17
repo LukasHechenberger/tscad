b1f8bc5886bf45659d3251500404288166f4ae8b
Lukas Hechenberger <5802656+LukasHechenberger@users.noreply.github.com>
2025-06-17 10:34:20 +0200

    style: Build exports

--- packages/modeling/tsup.config.ts
+++ packages/modeling/tsup.config.ts
@@ -1,5 +1,6 @@
+import { readFile, writeFile } from 'node:fs/promises';
+import path from 'node:path';
 import { defineConfig } from 'tsup';
-import path from 'path';
 
 export default defineConfig({
   entry: ['src/index.ts', 'src/*/*.ts'],
@@ -7,4 +8,45 @@ export default defineConfig({
   outDir: 'out',
   dts: true,
   sourcemap: true,
+  esbuildPlugins: [
+    {
+      name: 'add-exports',
+      setup({ onEnd }) {
+        onEnd(async ({ outputFiles }) => {
+          if (!outputFiles) {
+            console.warn('No output files found');
+            return;
+          }
+
+          const exports: [string, Record<string, string | Record<string, string>>][] = [];
+
+          for (const file of outputFiles) {
+            // Only handle esm files to just do it once
+            if (file.path.endsWith('.js')) {
+              const relativePath = `./${path.relative(process.cwd(), file.path)}`;
+              let exportPath = path.dirname(path.relative('out', relativePath));
+
+              if (!exportPath.startsWith('.')) exportPath = `./${exportPath}`;
+
+              exports.push([
+                exportPath,
+                {
+                  types: relativePath.replace(/\.js$/, '.d.ts'),
+                  default: relativePath,
+                },
+              ]);
+            }
+          }
+
+          if (exports.length > 0) {
+            const manifest = JSON.parse(await readFile('package.json', 'utf8'));
+            manifest.exports = Object.fromEntries(exports);
+            console.log('EXPORTS', manifest.exports);
+
+            await writeFile('package.json', `${JSON.stringify(manifest, null, 2)}\n`);
+          }
+        });
+      },
+    },
+  ],
 });
