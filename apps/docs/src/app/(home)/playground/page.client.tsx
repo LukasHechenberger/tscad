4c9bd31599f68639d61635618bbd8b9bb68c2bc0
Lukas Hechenberger <5802656+LukasHechenberger@users.noreply.github.com>
2025-06-20 09:43:03 +0200

    style: Better example

--- apps/docs/src/app/(home)/playground/page.client.tsx
+++ apps/docs/src/app/(home)/playground/page.client.tsx
@@ -93,11 +93,14 @@ const PlaygroundContext = createContext<PlaygroundContextType>(
   undefined as unknown as PlaygroundContextType,
 );
 
-const defaultCode = `import { cube, sphere } from '@tscad/modeling/primitives';  
+const defaultCode = `import { cube, sphere } from '@tscad/modeling/primitives';
 
 /** Use this method to create your model */
 export function main() {
-  return [cube({ size: 1.5 }), sphere({})];
+  return [
+    cube({ size: 1.5, center: [0, 0.75, 0] }),
+    sphere({ radius: 1.2, center: [0, 1.2, 0] }),
+  ];
 }`;
 
 const code$ = observable(defaultCode);
