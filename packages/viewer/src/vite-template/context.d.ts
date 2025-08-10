1d4cc4c034ffb38ac51b376935a3d7b47523938d
Lukas Hechenberger <5802656+LukasHechenberger@users.noreply.github.com>
2025-08-10 12:31:32 +0200

    chore: Fix typing

--- packages/viewer/src/vite-template/context.d.ts
+++ packages/viewer/src/vite-template/context.d.ts
@@ -1,6 +1,6 @@
-import type { Model } from '@tscad/modeling';
-
 declare module '@tscad-viewer/model' {
+  import type { Model } from '@tscad/modeling';
+
   const model: Model;
 
   export default model;
