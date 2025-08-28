2eb11b2a36558ffb65e0487f190a8daa120a0b89
Lukas Hechenberger <5802656+LukasHechenberger@users.noreply.github.com>
2025-08-28 05:30:40 +0200

    style: DRY

--- apps/docs/src/app/(home)/playground/page.tsx
+++ apps/docs/src/app/(home)/playground/page.tsx
@@ -13,23 +13,17 @@ export function generateMetadata() {
 }
 
 export default async function PlaygroundPage() {
-  const moduleTypes = [
-    {
-      moduleName: '@tscad/modeling/primitives',
-      source: await readFile('./node_modules/@tscad/modeling/out/primitives/index.d.ts', 'utf8'),
-    },
-    ...(await Promise.all(
-      Object.entries(modelingExports)
-        .filter(([, value]) => typeof value !== 'string')
-        .map(async ([key, value]) => ({
-          moduleName: path.join(modelingPackageName, key),
-          source: await readFile(
-            `./node_modules/@tscad/modeling/${(value as { default: string }).types}`,
-            'utf8',
-          ),
-        })),
-    )),
-  ];
+  const moduleTypes = await Promise.all(
+    Object.entries(modelingExports)
+      .filter(([, value]) => typeof value !== 'string')
+      .map(async ([key, value]) => ({
+        moduleName: path.join(modelingPackageName, key),
+        source: await readFile(
+          `./node_modules/@tscad/modeling/${(value as { default: string }).types}`,
+          'utf8',
+        ),
+      })),
+  );
 
   return (
     <PlaygroundProvider>
@@ -37,11 +31,11 @@ export default async function PlaygroundPage() {
 
       <div className="relative pt-[var(--fd-nav-height)] h-screen">
         <ResizablePanelGroup direction="horizontal" className="relative">
-          <ResizablePanel>
+          <ResizablePanel defaultSize={50}>
             <PlaygroundEditor moduleTypes={moduleTypes} />
           </ResizablePanel>
           <ResizableHandle />
-          <ResizablePanel>
+          <ResizablePanel defaultSize={50}>
             <PlaygroundPreview />
           </ResizablePanel>
         </ResizablePanelGroup>
