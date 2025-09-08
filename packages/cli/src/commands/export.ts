4555c17848655b1706602823fe3f9cba33df803b
Lukas Hechenberger <5802656+LukasHechenberger@users.noreply.github.com>
2025-09-08 07:46:57 +0200

    style: Support 3mf export

--- packages/cli/src/commands/export.ts
+++ packages/cli/src/commands/export.ts
@@ -1,13 +1,16 @@
 import { writeFile } from 'node:fs/promises';
-import { join } from 'node:path';
-import { serialize as toStl } from '@jscad/stl-serializer';
+import { extname, join, relative } from 'node:path';
+import * as threeMf from '@jscad/3mf-serializer';
+import * as stl from '@jscad/stl-serializer';
 import { Command } from '@tscad/commander';
 import { build } from 'esbuild';
+import open from 'open';
 
 export const exportCommand = new Command('export')
   .description('Export the model to a file')
   .argument('[model]', 'Where to find the tscad model', './src/model.ts')
-  .option('--output', 'The output file', 'out/model.stl')
+  .option('--output <filename>', 'The output file', 'out/model.3mf')
+  .option('--slice <slicer>', 'Open the result in a slicer', false)
   .action(async function runExportCommand(model, options) {
     const sourcePath = join(process.cwd(), model);
     const outPath = join(process.cwd(), options.output);
@@ -15,21 +18,38 @@ export const exportCommand = new Command('export')
     let importPath = sourcePath;
 
     if (model.endsWith('.ts')) {
-      importPath = outPath.replace(/\.stl$/, '.mjs');
+      importPath = outPath.replace(extname(outPath), '.mjs');
 
+      console.time('Build model');
       await build({
         entryPoints: [sourcePath],
         bundle: true,
         format: 'esm',
         outfile: importPath,
       });
+      console.timeEnd('Build model');
     }
 
+    console.time('Load model');
     const geometry = await import(importPath).then((module_) => module_.default);
+    console.timeEnd('Load model');
 
-    const stlData = toStl({ binary: true }, geometry);
+    const coder = outPath.endsWith('.3mf') ? threeMf : stl;
+    console.log('Using coder:', coder.mimeType);
 
-    const blob = new Blob(stlData, { type: 'application/sla' });
+    console.time('Serialize model');
+    const data = coder.serialize({ binary: true }, geometry);
+    console.timeEnd('Serialize model');
+
+    console.time('Write file');
+    const blob = new Blob(data, { type: coder.mimeType });
 
     await writeFile(outPath, Buffer.from(await blob.arrayBuffer()));
+    console.timeEnd('Write file');
+
+    console.log(`Exported model to ${relative(process.cwd(), outPath)}`);
+
+    if (options.slice) {
+      await open(outPath, { app: { name: options.slice } });
+    }
   });
