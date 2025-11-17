dd67b714edc62e7ee0672d6403e48429cd34ee59
Lukas Hechenberger <5802656+LukasHechenberger@users.noreply.github.com>
2025-11-17 16:39:46 +0100

    style: Use rsbuild-exec in cli

--- packages/cli/rslib.config.ts
+++ packages/cli/rslib.config.ts
@@ -1,8 +1,5 @@
-import { exec as _exec } from 'node:child_process';
-import { promisify } from 'node:util';
-import { defineConfig, RsbuildPlugin } from '@rslib/core';
-
-const exec = promisify(_exec);
+import { defineConfig } from '@rslib/core';
+import { pluginExec } from 'rsbuild-exec';
 
 export default defineConfig({
   source: {
@@ -18,23 +15,9 @@ export default defineConfig({
   },
   lib: [{ id: 'lib', format: 'esm' }],
   plugins: [
-    {
-      name: 'api-docs',
-      setup(api) {
-        api.onAfterEnvironmentCompile(async () => {
-          const start = performance.now();
-
-          try {
-            await exec('node --enable-source-maps ./out/scripts/update-documentation.js');
-
-            const end = performance.now();
-            api.logger.ready(`built in ${((end - start) / 1000).toFixed(2)} s (documentation)`);
-          } catch (error) {
-            api.logger.error('Failed to update documentation');
-            api.logger.error(error);
-          }
-        });
-      },
-    } satisfies RsbuildPlugin,
+    pluginExec({
+      title: 'api-docs',
+      command: 'node --enable-source-maps ./out/scripts/update-documentation.js',
+    }),
   ],
 });
