12a3461a1ed9735bb8d2771af8306d40b5ee7f23
Lukas Hechenberger <5802656+LukasHechenberger@users.noreply.github.com>
2025-11-17 20:13:41 +0100

    style: Leaner cli docs

--- packages/cli/src/scripts/update-documentation.ts
+++ packages/cli/src/scripts/update-documentation.ts
@@ -1,8 +1,8 @@
 import { existsSync } from 'node:fs';
 import { writeFile } from 'node:fs/promises';
-import { inspect, styleText } from 'node:util';
+import { styleText } from 'node:util';
 import { MarkdownTemplate, Template } from '@toolsync/template';
-import type { Argument, Command, Option } from '@tscad/commander';
+import type { Command } from '@tscad/commander';
 import { cli } from '../index';
 
 // NOTE: This script does not handle subcommands of subcommands
@@ -13,22 +13,39 @@ class RawTemplate extends Template {
   }
 }
 
-const getArgumentDescription = (option: Argument | Option) => [
-  ...(option.defaultValue ? [`Default: \`${inspect(option.defaultValue)}{:js}\``] : []),
-  option.description,
-];
-
 const getArgumentsDocumentation = (command: Command) =>
-  command.registeredArguments.flatMap((option) => [
-    `### \`${option.name()}\``,
-    ...getArgumentDescription(option),
-  ]);
+  `<TypeTable type={${JSON.stringify(
+    Object.fromEntries(
+      command.registeredArguments.map((argument) => [
+        argument.name(),
+        {
+          type: 'string',
+          required: argument.required,
+          description: argument.description,
+          default: argument.defaultValue,
+        },
+      ]),
+    ),
+    undefined,
+    2,
+  )}} />`;
 
 const getOptionsDocumentation = (command: Command) =>
-  command.options.flatMap((option) => [
-    `### \`${option.flags}\``,
-    ...getArgumentDescription(option),
-  ]);
+  `<TypeTable type={${JSON.stringify(
+    Object.fromEntries(
+      command.options.map((argument) => [
+        `--${argument.name()}`,
+        {
+          type: typeof argument.defaultValue,
+          required: argument.required,
+          description: argument.description,
+          default: argument.defaultValue,
+        },
+      ]),
+    ),
+    undefined,
+    2,
+  )}} />`;
 
 const logPrefix = styleText(['magenta'], 'DOC');
 console.time(`${logPrefix} ⚡️ Build succeeded`);
@@ -84,8 +101,14 @@ ${command
   template.update({
     section: 'arguments',
     content: [
+      "import { TypeTable } from 'fumadocs-ui/components/type-table';",
       ...(command.registeredArguments.length > 0
-        ? [`## Arguments`, ...getArgumentsDocumentation(command)]
+        ? [
+            '## API',
+            `\`${cli.name()} ${command.name()} ${command.usage()}\``,
+            `### Arguments [!toc]`,
+            getArgumentsDocumentation(command),
+          ]
         : ['{/* No arguments available. */}']),
     ].join('\n\n'),
     insert: 'bottom',
@@ -93,7 +116,7 @@ ${command
 
   template.update({
     section: 'options',
-    content: ['## Options', ...getOptionsDocumentation(command)].join('\n\n'),
+    content: ['### Options [!toc]', getOptionsDocumentation(command)].join('\n\n'),
     insert: 'bottom',
   });
 
